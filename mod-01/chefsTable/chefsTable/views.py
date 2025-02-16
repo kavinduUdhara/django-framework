793ab146c22a02f66addbcf7079976ccaeb05572
kavinduUdhara <wmkavinduudhara@gmail.com>
2025-02-16 12:23:02 +0530

    completed view section of mod2

--- mod-01/chefsTable/chefsTable/views.py
+++ mod-01/chefsTable/chefsTable/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
+    path = request.path
     content = "<html><body><h1>welcome</h1></body></html>"
-    return HttpResponse(content)
+    return HttpResponse(str(path))
